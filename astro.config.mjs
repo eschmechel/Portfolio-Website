c686f22719b2ce59fb1386631cc40e60e52fd2e5
Elliott Schmechel <elliottschmechel@gmail.com>
2025-10-02 02:21:19 -0700

    feat: Update project dependencies and add new components
    - Updated package.json to include new dependencies for Astro, React, Tailwind CSS, and TypeScript.
    - Redesigned favicon.svg to feature a gradient background and letter "E".
    - Refactored index.astro to use a Base layout and include Hero and Work components.
    - Enhanced tsconfig.json for React JSX support.
    - Added robots.txt for SEO optimization.
    - Introduced GithubChart component to display GitHub contributions.
    - Created Hero component for the landing page with a call-to-action.
    - Developed Work component to showcase featured projects with detailed descriptions.
    - Implemented Footer component with social links and quick navigation.
    - Added NavBar component for site navigation.
    - Created SocialsNav component for social media links with customizable labels.
    - Established Base layout for consistent page structure and SEO metadata.
    - Created about.astro and experiences.astro pages with Base layout.
    - Developed colors.astro page to test and display color scheme.
    - Added global CSS styles for consistent theming and component styling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eschmechel.dev',
+  base: '/',
+  trailingSlash: 'ignore',
+  output: 'static',
+  prefetch: true,
+
+  integrations: [sitemap(), icon(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
